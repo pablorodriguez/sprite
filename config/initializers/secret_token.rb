86435b0fb587a8abdc9100d8ed74633d8edf01c5
Pablo Rodriguez <pablo@comunidadbox.com>
2013-08-07 14:21:50 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sprites::Application.config.secret_token = '71b351f6cd367a6c5a9664b61863a2a4a5493b11e2d56639c66f89929b26bcd88c4544cccff345b6a51d93ad444ea29910c4b0cf442042a97e5ec883ba74206f'
